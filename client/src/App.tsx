0d4f013a9be86f35ba7dc6e5b178cf81bd926c7c
Qreamville <Oyeleyebalikis@yahoo.com>
2022-09-06 00:21:00 -1200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
-function App() {
+import React from "react";
+import Donators from "./components/Donators";
+import Donation from "./components/Donation";
+
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <main className="h-screen w-full bg-[#100e34] grid place-items-center px-2">
+      <div className="bg-[#100e49] md:w-[800px] rounded-lg text-white/80 flex flex-col gap-y-6 md:flex-row justify-between px-6 items-center py-4 min-h-[300px]">
+        <Donation />
+        <Donators />
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
