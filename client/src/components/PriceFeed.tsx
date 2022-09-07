c9f42ae80660c4520484b630e4387bc07438fde6
Qreamville <Oyeleyebalikis@yahoo.com>
2022-09-07 04:14:31 -1200

    Update PriceFeed.tsx

--- client/src/components/PriceFeed.tsx
+++ client/src/components/PriceFeed.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { useGlobalContext } from "../context/DonationsContext";
 
 type amountProps = {
   amounts: number;
 };
 
 const PriceFeed = ({ amounts }: amountProps) => {
+  const { amountToDonate } = useGlobalContext();
   return (
-    <div className="border border-[#100e34] px-2 py-1 rounded-xl  hover:bg-white hover:text-[#100e34] cursor-pointer">
-      <span className="text-sm md:text-base font-bold">{amounts} ETH</span>
+    <div
+      className="border border-[#100e34] px-2 py-1 rounded-xl  hover:bg-white hover:text-[#100e34] cursor-pointer"
+      onClick={() => amountToDonate(amounts)}
+    >
+      <span className="text-sm md:text-base font-bold">{amounts} WEI</span>
     </div>
   );
 };
