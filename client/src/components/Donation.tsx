3e29ad1f5e38948d93893b594751c41fd3337ef8
Qreamville <Oyeleyebalikis@yahoo.com>
2022-09-07 04:14:26 -1200

    Update Donation.tsx

--- client/src/components/Donation.tsx
+++ client/src/components/Donation.tsx
@@ -1,15 +1,21 @@
+import { useGlobalContext } from "../context/DonationsContext";
 import PriceFeed from "./PriceFeed";
 
 const Donation = () => {
-  const amounts = [0.1, 0.5, 1, 2].map((amount, _index) => {
+  const { donate } = useGlobalContext();
+
+  const amounts = [10, 50, 20, 60].map((amount, _index) => {
     return <PriceFeed amounts={amount} key={_index} />;
   });
 
   return (
     <div className="flex flex-col gap-y-4">
-      <p className="text-lg font-semibold">Select amount(in ETH)</p>
+      <p className="text-lg font-semibold">Select amount(in Wei)</p>
       <div className="flex gap-x-4">{amounts}</div>
-      <button className="bg-red-700 py-3 font-bold hover:bg-red-600">
+      <button
+        className="bg-red-700 py-3 font-bold hover:bg-red-600"
+        onClick={donate}
+      >
         Donate Now
       </button>
     </div>
