f5b3c88f661e9830ae111fbaeae316c4d6071425
Qreamville <Oyeleyebalikis@yahoo.com>
2022-09-07 04:15:00 -1200

    Update deploy.ts

--- smart-contract/scripts/deploy.ts
+++ smart-contract/scripts/deploy.ts
@@ -17,30 +17,14 @@ const verify = async (contractAddress: string, args: any[]) => {
 };
 
 async function main() {
-  const [owner, signer1, signer2, signer3] = await ethers.getSigners();
+  const [owner] = await ethers.getSigners();
 
   const Donate = await ethers.getContractFactory("Donate", owner);
   const donate = await Donate.deploy();
   await donate.deployed();
-  // await verify(donate.address, []);
+  await verify(donate.address, []);
   console.log(`deployed to ${donate.address} by ${owner.address}`);
-  // owner: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
-  // contract.address: 0x5FbDB2315678afecb367f032d93F642f64180aa3
-
-  await signer1.sendTransaction({
-    to: donate.address,
-    value: ethers.utils.parseUnits("0.1", 18),
-  });
-
-  await signer1.sendTransaction({
-    to: donate.address,
-    value: ethers.utils.parseUnits("0.1", 18),
-  });
-
-  await signer1.sendTransaction({
-    to: donate.address,
-    value: ethers.utils.parseUnits("0.1", 18),
-  });
+  // contract.address: 0x1C89D3e5C78fcBE10E2277Bd2BDa4302720DE599
 }
 
 main().catch((error) => {
